b73fdf06b8f05bd5091b6fd868e3c546fade768d
Mohamad Matar <mohamad.matar@student.arteveldehs.be>
2024-04-30 14:05:32 +0200

    buttons

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="/assets/images/logo/png/logo-no-background.png" class="h-14 w-auto fill-current text-gray-800 dark:text-gray-200" alt="Var logo">
+<img src="/assets/images/logo/png/logo-no-background.png" {{ $attributes }} alt="{{ config('app.name') }}" />
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title', config('app.name'))</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/assets/favicon/site.webmanifest">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
